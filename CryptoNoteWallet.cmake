77fc7c67a5c9cffab6f39882d97043f8f266b6dc
XeroXipher <83317281+XeroXipher@users.noreply.github.com>
2021-08-24 00:23:48 -0300

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
+set(CN_PROJECT_NAME "CashFlowTransactionToken")
+set(CN_CURRENCY_DISPLAY_NAME "Cash Flow Transaction Token")
 set(CN_CURRENCY_TICKER "")
